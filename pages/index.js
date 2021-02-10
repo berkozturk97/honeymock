7c256a47ded562fcd4006b895745a9601cfb4358
Berk Ozturk <34316304+berkozturk97@users.noreply.github.com>
2021-02-10 23:27:51 +0300

    home page

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import AppFooter from '../components/Nav/footer'
 import '../components/home.less'
+import { Text } from '@chakra-ui/react'
+import { Button } from '@material-ui/core'
 
 function HomePage() {
   const router = useRouter()
   const [isVisible, setIsVisible] = useState(false)
-  
-   return (
+
+  return (
     <div>
-        <div className={'bg'}>
-          
+      <div className={'bg'}>
+        <div className="initialText">
+          <Text fontSize="6xl">Choose a job love</Text>
+          <Text mt={10} fontSize="3xl">Join the employment marketplace for tech professionals where you're matched with companies that apply to you. </Text>
         </div>
-        <AppFooter></AppFooter>
+        <div className="buttons">
+            <Button variant="contained" color="primary" fullWidth>Find me a job</Button>
+            <Button variant="outlined" fullWidth>I'm hiring</Button>
+        </div>
+      </div>
+      <AppFooter></AppFooter>
     </div>
-    )
-  
-  
+  )
 }
 
 export default HomePage
