8faed750411471da5688adc1e68224de4f020b8b
Seçkin Özdemir <seckin.ozdemir@getir.com>
2021-02-13 16:26:27 +0300

    navigation

--- components/Nav/navigation.js
+++ components/Nav/navigation.js
@@ -110,15 +110,17 @@ export default function Navigation() {
           query: {
             id: user ? user._id : null,
           }
-      });
-        // return `/talentProfile/?id=${user ? user._id : null}`;
-      }
-      router.push({
+        });
+      }else{
+        router.push({
         pathname: '/companyProfile',
         query: {
           id: user ? user._id : null,
         }
-    });
+       }); 
+      }
+   
+      // return `/talentProfile/?id=${user ? user._id : null}`;
       // return `/company/companyProfile/?id=${user ? user._id : null}`;
     }
     return '/';
@@ -156,7 +158,7 @@ export default function Navigation() {
           <Button className={classes.button} href="/" color="inherit">For Talent</Button>
           <Button className={classes.button} href="/employer" color="inherit">For Employers</Button>
           <Button className={classes.button} href="/aboutus" color="inherit">About Us</Button>
-          <Button onClick={goProfile()} color="inherit">{`${user ? user.name : null} ${user ? user.surname : ''}`}</Button>
+          <Button onClick={()=> goProfile()} color="inherit">{`${user ? user.name : null} ${user ? user.surname : ''}`}</Button>
           <Button onClick={logOut} color="inherit">Logout</Button>
         </>
       )
@@ -197,7 +199,7 @@ export default function Navigation() {
             <Button className={classes.button} href="/aboutus" color="inherit">About Us</Button>
           </MenuItem>
           <MenuItem>
-            <Button className={classes.button} href={goProfile()} color="inherit">{`${user? user.name : null} ${user ? user.surname : ''}`}</Button>
+            <Button className={classes.button} onClick={()=> goProfile()} color="inherit">{`${user? user.name : null} ${user ? user.surname : ''}`}</Button>
           </MenuItem>
           <MenuItem>
             <Button className={classes.button} onClick={logOut} color="inherit">Logout</Button>
